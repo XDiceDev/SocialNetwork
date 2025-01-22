4b40e15d5110ac728f67ac2f05597a6eda1f987f
LiquidDevQ <72251079+XDiceDev@users.noreply.github.com>
2025-01-22 22:00:26 +1000

    Created 2 GET endpoints and 1 POST endpoint

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,7 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'), #Первый роут, запускает функцию index из views
+    path('get-endpoint-1/', views.get_endpoint_1, name='get_endpoint_1'), # Первый GET эндпоинт
+    path('get-endpoint-2/', views.get_endpoint_2, name='get_endpoint_2'), # Второй GET эндпоинт
+    path('post-endpoint/', views.post_endpoint, name='post_endpoint'), # POST эндпоинт
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return HttpResponse("Тестовая страница") #Возвращает http ответ с текстом "Тестовая страница"
+
+def get_endpoint_1(request):
+    return HttpResponse("Это первый GET эндпоинт")
+
+def get_endpoint_2(request):
+    return HttpResponse("Это второй GET эндпоинт")
+
+@csrf_exempt #Отключает проверку CSRF, чтобы тестировать запросы
+def post_endpoint(request):
+    if request.method == 'POST':
+        data = {"message": "POST запрос"}
+        return JsonResponse(data)
+    return JsonResponse({"message": "Неправильный метод запроса"})
